d299a5d610c5b2c4a812e2c6a41899e1622d38ab
Daniel <ddelachica89@gmail.com>
2020-05-24 12:12:13 -0500

    added models

--- grocery_planner_proj/grocery_planner_app/models.py
+++ grocery_planner_proj/grocery_planner_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+import re
 
-# Create your models here.
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        email_checker = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not email_checker.match(postData['email']):
+            errors['email'] = 'Must be a valid email'
+        if postData['pass'] != postData['confirm']:
+            errors['pass'] = 'Nope, your passwords did not match. Try again.'
+        return errors
+    def login_validator(self, postData):
+        errors = {}
+        email_checker = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not email_checker.match(postData['email']):
+            errors['email'] = 'Must be a valid email'
+        return errors
+
+class User(models.Model):
+    user_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
+    objects = UserManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
