594b93907da6a12747a47368a4096e9b0cd5f1c7
John Green <johngengreen@gmail.com>
2020-05-25 15:05:57 -0700

    Dashboard added
    Added the dashboard html, css, js.  Site name is included in index. Favicon is added.

--- grocery_planner_proj/grocery_planner_app/views.py
+++ grocery_planner_proj/grocery_planner_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
